b3040d03931b137776bf18b7c54f106e3cc3b946
Rodrigo Vanegas <rvanegas@gmail.com>
2022-01-25 14:55:07 -0800

    add dnd

--- src/DragTest.js
+++ src/DragTest.js
@@ -1,27 +1,44 @@
 import React from 'react'
-import { useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
 
 function Box() {
-  const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
+  const [{isDragging, canDrag}, drag, dragPreview] = useDrag(() => ({
     // "type" is required. It is used by the "accept" specification of drop targets.
     type: 'BOX',
     // The collect function utilizes a "monitor" instance (see the Overview for what this is)
     // to pull important pieces of state from the DnD system.
+    end() {
+      console.log('ended')
+    },
     collect: (monitor) => ({
-      isDragging: monitor.isDragging()
+      isDragging: monitor.isDragging(),
+      canDrag: monitor.canDrag(),
     })
   }))
 
-  return (
+  // return (
+  //   <div ref={dragPreview} style={{ opacity: isDragging ? 0.5 : 1}}>
+  //       {/* The drag ref marks this node as being the "pick-up" node */}
+  //       <div ref={drag} >
+  //         foo {canDrag ? 'bar' : 'X'}
+  //       </div>
+  //   </div>
+  // )
+
+  return isDragging ? (
     <div ref={dragPreview} style={{ opacity: isDragging ? 0.5 : 1}}>
-        {/* The drag ref marks this node as being the "pick-up" node */}
-        <div role="Handle" ref={drag} />
+      foo {canDrag ? 'Z' : 'W'}
+    </div>
+  ) : (
+    <div ref={drag} >
+      bar {canDrag ? 'Y' : 'X'}
     </div>
   )
 }
 
 function Bucket() {
-  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+  const [{isOver, canDrop}, drop] = useDrop(() => ({
     // The type (or types) to accept - strings or symbols
     accept: 'BOX',
     // Props to collect
@@ -34,8 +51,7 @@ function Bucket() {
   return (
     <div
       ref={drop}
-      role={'Dustbin'}
-      style={{ backgroundColor: isOver ? 'red' : 'white' }}
+      style={{backgroundColor: isOver ? 'red' : 'white'}}
     >
       {canDrop ? 'Release to drop' : 'Drag a box here'}
     </div>
@@ -44,9 +60,9 @@ function Bucket() {
 
 export default function DragTest() {
   return (
-    <div>
+    <DndProvider backend={HTML5Backend}>
       <Bucket/>
       <Box/>
-    </div>
+    </DndProvider>
   )
 }
