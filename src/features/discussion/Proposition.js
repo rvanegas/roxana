649281ef066ea9f9da7d82228472817ba7ce29a5
Rodrigo Vanegas <rvanegas@gmail.com>
2022-01-24 21:11:09 -0800

    dispatch writes to store

--- src/features/discussion/Proposition.js
+++ src/features/discussion/Proposition.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-import {
-  View, Button, Divider
-} from '@aws-amplify/ui-react'
+import { useDispatch } from 'react-redux'
+import { View, Button, Divider } from '@aws-amplify/ui-react'
 import { Editor, EditorState, ContentState } from 'draft-js';
+import { updateProposition } from './propositionsSlice'
 
 export function Proposition(props) {
-  const [editorState, setEditorState] = useState(() => {
+  function initEditorState() {
     const contentState = ContentState.createFromText(props.proposition.content)
     return EditorState.createWithContent(contentState)
-  })
-  const [readOnly, setReadOnly] = useState(false)
+  }
   function plainText() {
     return editorState.getCurrentContent().getPlainText()
   }
   function reset() {
     setEditorState(EditorState.createWithContent(editorState.getCurrentContent()))
   }
+  function handleUpdate() {
+    const id = props.proposition.id
+    const content = plainText()
+    dispatch(updateProposition({id, content}))
+  }
+
+  const [editorState, setEditorState] = useState(initEditorState)
+  const [readOnly, setReadOnly] = useState(false)
+  const dispatch = useDispatch()
 
   return (
     <View>
-      <Button onClick={() => {setReadOnly(!readOnly)}}>edit</Button>
-      <Button onClick={() => {console.log(plainText())}}>text</Button>
-      <Button onClick={() => {reset()}}>reset</Button>
+      <Button onClick={() => setReadOnly(!readOnly)}>edit</Button>
+      <Button onClick={() => console.log(plainText())}>text</Button>
+      <Button onClick={() => reset()}>reset</Button>
+      <Button onClick={() => handleUpdate()}>dispatch</Button>
       <Divider/>
       [{readOnly ? '1' : '0'}]
       <Divider/>
