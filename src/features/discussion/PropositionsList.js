d1c554833fdc8ed33247b7203bb89c135b52d4a1
Rodrigo Vanegas <rvanegas@gmail.com>
2022-02-14 13:41:19 -0800

    moved PropositionList component to discussionSlice

--- src/features/discussion/PropositionsList.js
+++ src/features/discussion/PropositionsList.js
@@ -4,51 +4,50 @@ import {useSelector, useDispatch} from 'react-redux'
 import {Text, Button, View, Heading} from '@aws-amplify/ui-react'
 import {Proposition} from './Proposition'
 import * as subscriptions from '../../graphql/subscriptions'
-import * as mutations from '../../graphql/mutations'
 import {
-  selectPropositions,
-  selectPropositionsStatus,
-  fetchPropositions,
-  updateProposition,
   focusOnPropositionThunk,
   clearPropositionsThunk
 } from './propositionsSlice'
 import {
+  selectDiscussion,
   getDiscussion
 } from './discussionsSlice'
 
+const discussionId = 'd172f9ff-8e5b-4229-b803-aee6dc8855a2'
+
 export function PropositionsList() {
   const dispatch = useDispatch()
-  const propositions = useSelector(selectPropositions)
-  const propositionsStatus = useSelector(selectPropositionsStatus)
+  const discussion = useSelector(selectDiscussion)
+  const propositions = discussion.propositions
+  const discussionStatus = discussion.status
   const [readOnly] = useState(false)
   const propositionsEmpty = propositions.length === 0
 
   function handleButton() {
-    dispatch(getDiscussion('d172f9ff-8e5b-4229-b803-aee6dc8855a2'))
+    dispatch(getDiscussion(discussionId))
   }
 
   useEffect(() => {
-    if (propositionsStatus === 'idle') {
+    if (discussionStatus === 'idle') {
       console.log('idle')
-      dispatch(fetchPropositions())
-    } else if (propositionsStatus === 'succeeded' && propositionsEmpty) {
+      dispatch(getDiscussion(discussionId))
+    } else if (discussionStatus === 'succeeded' && propositionsEmpty) {
       console.log('succeeded')
-      dispatch(focusOnPropositionThunk(0))
+      // dispatch(focusOnPropositionThunk(0))
     }
 
     const subscription = API.graphql(graphqlOperation(subscriptions.onUpdateDiscussion)).subscribe({
       next: next => {
         console.log('next', next)
-        dispatch(getDiscussion(next.value.data.onUpdateDiscussion))
+        dispatch(getDiscussion(next.value.data.onUpdateDiscussion.id))
       },
-      error: error => console.warn(error)
+      error: error => console.error(error)
     })
 
     return function cleanup() {
       subscription.unsubscribe()
     }
-  }, [dispatch, propositionsStatus, propositionsEmpty])
+  }, [dispatch, discussionStatus, propositionsEmpty])
 
   const propositionEntities = propositions.map(proposition => (
     <React.Fragment key={proposition.id}>
