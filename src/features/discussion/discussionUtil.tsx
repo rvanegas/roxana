a5b60ad142d5d6b2cfb718e9f1f873a0915e6374
Rodrigo Vanegas <rvanegas@gmail.com>
2022-03-20 15:27:52 -0700

    simplify claimsSummary

--- src/features/discussion/discussionUtil.tsx
+++ src/features/discussion/discussionUtil.tsx
@@ -1,34 +1,22 @@
 import {Sentence} from './discussion.d'
 
 export function claimsSummary(sentence: Sentence, discussants: string[]) : boolean[] {
-  let acceptions = 0
-  let rejections = 0
-
-  for (let discussant of discussants) {
-    if (sentence.accepted.includes(discussant)) {
-      acceptions++
-    }
-    if (sentence.rejected.includes(discussant)) {
-      rejections++
-    }
-  }
-
-  if (acceptions > 0 && rejections > 0) {
+  if (sentence.accepted.length > 0 && sentence.rejected.length > 0) {
     return [true, false]
   }
-  if (acceptions > 1) {
+  if (sentence.accepted.length > 1) {
     return [true, true]
   }
-  if (rejections > 1) {
+  if (sentence.rejected.length > 1) {
     return [false, false]
   }
-  if (acceptions === 1) {
+  if (sentence.accepted.length === 1) {
     return [true]
   }
-  if (rejections === 1) {
+  if (sentence.rejected.length === 1) {
     return [false]
   }
-  if (acceptions === 0 && rejections === 0) {
+  if (sentence.accepted.length === 0 && sentence.rejected.length === 0) {
     return []
   }
 
