131f1a4ef39023c1f2df6f8761a31099637b0d70
Rodrigo Vanegas <rvanegas@gmail.com>
2022-03-30 22:58:12 -0700

    0.5.4 bugsnag

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
 import {AmplifyProvider, Authenticator} from '@aws-amplify/ui-react'
-import Amplify from "aws-amplify";
-import {Provider} from 'react-redux';
-import App from './App';
-import {store} from './app/store';
+import Amplify from 'aws-amplify'
+import {Provider} from 'react-redux'
+import App from './App'
+import {store} from './app/store'
 import {theme} from './theme'
-import * as serviceWorker from './serviceWorker';
-import awsExports from "./aws-exports";
+import * as serviceWorker from './serviceWorker'
+import awsExports from "./aws-exports"
 const packageJson = require('../package.json')
 
 Amplify.configure(awsExports);
 console.log('version', packageJson.version)
 
+Bugsnag.start({
+  apiKey: 'f9ee89b75415659a9708bea10e368d0f',
+  plugins: [new BugsnagPluginReact()]
+})
+
+// @ts-ignore
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AmplifyProvider theme={theme}>
-        <Authenticator.Provider>
-          <App />
-        </Authenticator.Provider>
-      </AmplifyProvider>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <AmplifyProvider theme={theme}>
+          <Authenticator.Provider>
+            <App />
+          </Authenticator.Provider>
+        </AmplifyProvider>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
