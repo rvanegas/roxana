1506687102923d59f999752f9a49d2ccd3972b99
Rodrigo Vanegas <rvanegas@gmail.com>
2024-01-24 23:48:40 -0800

    move readonly up, no worry about undo history

--- src/features/discussion/PropositionsList.js
+++ src/features/discussion/PropositionsList.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import {
-  View, Button
+  View, Button, Divider
 } from '@aws-amplify/ui-react'
 import { Proposition } from './Proposition'
 import { selectPropositions } from './propositionsSlice'
@@ -9,13 +9,19 @@ import { selectPropositions } from './propositionsSlice'
 
 export function PropositionsList() {
   const propositions = useSelector(selectPropositions)
+  const [readOnly, setReadOnly] = useState(false)
+
   const propositionEntities = propositions.map(proposition => (
-    <Proposition key={proposition.id} proposition={proposition} />
+    <Proposition key={proposition.id} proposition={proposition} readOnly={readOnly} />
   ))
 
   return (
     <View>
       <Button onClick={() => {console.log(propositions)}}>state</Button>
+      <Button onClick={() => setReadOnly(!readOnly)}>edit</Button>
+      <Divider/>
+      [{readOnly ? '1' : '0'}]
+      <Divider/>
       {propositionEntities}
     </View>
   )
