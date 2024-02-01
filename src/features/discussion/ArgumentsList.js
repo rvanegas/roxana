55f00b8fe24714213fa4afb08041787d90a57fda
Rodrigo Vanegas <rvanegas@gmail.com>
2024-01-31 23:24:18 -0800

    remove edit switches

--- src/features/discussion/ArgumentsList.js
+++ src/features/discussion/ArgumentsList.js
@@ -1,16 +1,12 @@
 import React, {useState} from 'react'
 import {useSelector} from 'react-redux'
-import {Grid, View, SwitchField} from '@aws-amplify/ui-react'
+import {Grid} from '@aws-amplify/ui-react'
 import {selectArguments} from './argumentsSlice'
 import {Argument} from './Argument'
 
 export function ArgumentsList() {
-  const [readOnly, setReadOnly] = useState(false)
+  const [readOnly] = useState(false)
   const arguments_ = useSelector(selectArguments)
-
-  function handleChange(e) {
-    setReadOnly(e.target.checked)
-  }
 
   if (!arguments_) {
     return null;
@@ -25,9 +21,6 @@ export function ArgumentsList() {
       templateColumns="1rem 2rem 1fr"
       gap="var(--amplify-space-small)"
     >
-      <View as="header" columnStart={2} columnEnd={-1}>
-        <SwitchField label="Read Only" defaultChecked={readOnly} onChange={handleChange} />
-      </View>
       {argumentElements}
     </Grid>
   )
