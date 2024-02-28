6c4557f7324fef705a54fc72d8b38533a40e80a2
Rodrigo Vanegas <rvanegas@gmail.com>
2024-02-28 04:33:04 -0800

    use query instead of path

--- src/app/util.js
+++ src/app/util.js
@@ -1,5 +1,5 @@
-
 import cryptoRandomString from 'crypto-random-string'
+import parse from 'url-parse'
 
 export async function sleep(ms) {
   console.log('sleep', ms)
@@ -8,4 +8,12 @@ export async function sleep(ms) {
 
 export function generateShortId() {
   return cryptoRandomString({length: 6, type: 'distinguishable'}).toLowerCase();
+}
+
+export function discussionIdFromQuery() {
+  return parse(window.location.href, true).query.d
+}
+
+export function redirectToDiscussionIdQuery(discussionId) {
+  window.location.search = `?d=${discussionId}`
 }
