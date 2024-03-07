b4269e4696c4dfdd0a4b2d58c98f49a4b35c9580
Rodrigo Vanegas <rvanegas@gmail.com>
2024-03-07 00:33:23 -0500

    disable readonly

--- src/features/discussion/PropositionsList.js
+++ src/features/discussion/PropositionsList.js
@@ -7,25 +7,17 @@ import {Proposition} from './Proposition'
 import '@aws-amplify/ui-react/styles.css'
 import {
   selectDiscussions,
-  propositionIdsFromArgument,
 } from './discussionsSlice'
 
 export function PropositionsList() {
   // const currentUser = useContext(CurrentUserContext)
   const discussions = useSelector(selectDiscussions)
   const propositions = discussions.propositions
-  const arguments_ = discussions.arguments || []
-  // can be memoized
-  const propositionIds = arguments_.map(a => propositionIdsFromArgument(a)).flat()
-  function isReadOnly(proposition) {
-    return propositionIds.some(id => id === proposition.id)
-  }
-
   // console.log('username', currentUser.username)
 
   const propositionEntities = !propositions ? null : propositions.map((proposition, position) => (
     <Proposition key={proposition.key} position={position} discussionId={discussions.discussionId}
-      proposition={proposition} readOnly={isReadOnly(proposition)}
+      proposition={proposition}
     />
   ))
 
