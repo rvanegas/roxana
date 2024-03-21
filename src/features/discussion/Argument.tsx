4cb4642f93b62679625ec0ec8037d7d5e6e407ef
Rodrigo Vanegas <rvanegas@gmail.com>
2024-03-21 12:02:25 -0700

    space btw arguments and special exception type for failed getdiscussion

--- src/features/discussion/Argument.tsx
+++ src/features/discussion/Argument.tsx
@@ -153,6 +153,7 @@ export function Argument({position, argument, discussionId}) {
   const postSentence = (
     <React.Fragment>
       {propositionElements}
+      <View style={{paddingBottom: '10px'}} columnSpan={4} />
     </React.Fragment>
   )
 
