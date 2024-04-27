282614fb93e0e3101fe0fce3f94ff44fdec36b34
Rodrigo Vanegas <rvanegas@gmail.com>
2024-04-27 11:18:27 -0700

    scan users

--- cli/index.tsx
+++ cli/index.tsx
@@ -1,7 +1,10 @@
-import { DynamoDBClient, ListTablesCommand } from '@aws-sdk/client-dynamodb'
+import {DynamoDBClient, ListTablesCommand, ScanCommand} from '@aws-sdk/client-dynamodb'
+
+// npx tsc index.tsx && node index.js
+
+const client = new DynamoDBClient({region: 'us-west-2'})
 
 async function tables() {
-  const client = new DynamoDBClient({ region: "us-west-2" });
   const command = new ListTablesCommand({});
   try {
     const results = await client.send(command);
@@ -11,4 +14,15 @@ async function tables() {
   }
 }
 
-tables();
+async function scanUsers() {
+  // User-u7epzlbjkngxjlz5cpiql2qium-roxana
+  const command = new ScanCommand({TableName: 'User-u7epzlbjkngxjlz5cpiql2qium-roxana'})
+  try {
+    const results = await client.send(command);
+    console.log(results.Items.map(item => item.username.S));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+scanUsers();
